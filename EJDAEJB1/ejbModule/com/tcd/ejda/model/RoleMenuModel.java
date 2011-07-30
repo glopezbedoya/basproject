ee88b98dcb065fc59336cae461114ca71a15a751
nuhpui <nuhpui@gmail.com>
2011-07-30 14:37:39 +0000

    Role for menu

---
+++ EJDAEJB1/ejbModule/com/tcd/ejda/model/RoleMenuModel.java
@@ -0,0 +1,92 @@
+package com.tcd.ejda.model;
+
+public class RoleMenuModel {
+	
+	private String JDA_ID;
+	private String IV_USER;
+	private String USER_NAME;
+
+	private String DEPARTMENT;
+	private String USER_STATUS;
+	  
+	private String ROLE_ID;
+	private String ROLE_FUN_ID;
+	private String MENU_ID ;
+	private String FUN_ADD;
+	private String FUN_UPDATE;
+	private String FUN_DELETE;
+	private String FUN_INQUIRY;
+	public String getJDA_ID() {
+		return JDA_ID;
+	}
+	public void setJDA_ID(String jDAID) {
+		JDA_ID = jDAID;
+	}
+	public String getIV_USER() {
+		return IV_USER;
+	}
+	public void setIV_USER(String iVUSER) {
+		IV_USER = iVUSER;
+	}
+	public String getUSER_NAME() {
+		return USER_NAME;
+	}
+	public void setUSER_NAME(String uSERNAME) {
+		USER_NAME = uSERNAME;
+	}
+	public String getDEPARTMENT() {
+		return DEPARTMENT;
+	}
+	public void setDEPARTMENT(String dEPARTMENT) {
+		DEPARTMENT = dEPARTMENT;
+	}
+	public String getUSER_STATUS() {
+		return USER_STATUS;
+	}
+	public void setUSER_STATUS(String uSERSTATUS) {
+		USER_STATUS = uSERSTATUS;
+	}
+	public String getROLE_ID() {
+		return ROLE_ID;
+	}
+	public void setROLE_ID(String rOLEID) {
+		ROLE_ID = rOLEID;
+	}
+	public String getROLE_FUN_ID() {
+		return ROLE_FUN_ID;
+	}
+	public void setROLE_FUN_ID(String rOLEFUNID) {
+		ROLE_FUN_ID = rOLEFUNID;
+	}
+	public String getMENU_ID() {
+		return MENU_ID;
+	}
+	public void setMENU_ID(String mENUID) {
+		MENU_ID = mENUID;
+	}
+	public String getFUN_ADD() {
+		return FUN_ADD;
+	}
+	public void setFUN_ADD(String fUNADD) {
+		FUN_ADD = fUNADD;
+	}
+	public String getFUN_UPDATE() {
+		return FUN_UPDATE;
+	}
+	public void setFUN_UPDATE(String fUNUPDATE) {
+		FUN_UPDATE = fUNUPDATE;
+	}
+	public String getFUN_DELETE() {
+		return FUN_DELETE;
+	}
+	public void setFUN_DELETE(String fUNDELETE) {
+		FUN_DELETE = fUNDELETE;
+	}
+	public String getFUN_INQUIRY() {
+		return FUN_INQUIRY;
+	}
+	public void setFUN_INQUIRY(String fUNINQUIRY) {
+		FUN_INQUIRY = fUNINQUIRY;
+	}
+	
+}
