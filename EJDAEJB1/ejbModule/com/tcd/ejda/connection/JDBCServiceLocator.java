b710749f4598a8c12fc0115c0b0d277e8d259d67
nuhpui <nuhpui@gmail.com>
2011-09-03 18:54:22 +0000

    Veena lookup ejb pool

--- EJDAEJB1/ejbModule/com/tcd/ejda/connection/JDBCServiceLocator.java
+++ EJDAEJB1/ejbModule/com/tcd/ejda/connection/JDBCServiceLocator.java
@@ -6,18 +6,13 @@ import java.util.Hashtable;
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+import org.apache.log4j.Logger;
+
 
 public class JDBCServiceLocator {
-
+	private Logger log = Logger.getLogger(JDBCServiceLocator.class);
 	 public static JDBCServiceLocator serviceLocator;
-//	 Connection conn=db.dbConnect("jdbc:oracle:thin:@localhost:1521/orcl", "SYSTEM", "orcl");
-//	    private final static String JAVA_ENV = "java:comp/env/";
-	    private final static String JAVA_ENV = "jdbc:oracle:thin:@localhost:1521/orcl";
-//	    public final static int MASTER_DB = 0;
-//	    public final static int LS_DB = 1;
-	    
-	    public final static String MASTER_DATA_SOURCE = "jdbc/master";
-//	    public final static String LS_DATA_SOURCE = "jdbc/lis";
+	    private final static String JAVA_ENV = "jdbc/ejdadb1";
 
 	    private static int clients = 0;
 	    private static Hashtable pools;
@@ -36,32 +31,19 @@ public class JDBCServiceLocator {
 	     * @return DataSource.getConnection()
 	     * @throws Exception
 	     */
-		public Connection getConnection(int dbType) throws Exception{
+		public Connection getConnection() throws Exception{
 			DataSource ds = null;
 			try{
 				InitialContext ctx = new InitialContext();
 				Object obj = null;
+				log.debug("JAVA_ENV >> " + JAVA_ENV);
 				ds = (DataSource)ctx.lookup(JAVA_ENV);
-//				switch(dbType){
-//				case MASTER_DB:
-//					System.out.println("Look UP >>>>>>>>> "+JAVA_ENV+MASTER_DATA_SOURCE);
-//					ds = (DataSource)ctx.lookup(JAVA_ENV+MASTER_DATA_SOURCE);
-//					break;
-//				case LS_DB:
-//					System.out.println("Look UP >>>>>>>>> "+JAVA_ENV+LS_DATA_SOURCE);
-//					ds = (DataSource)ctx.lookup(JAVA_ENV+LS_DATA_SOURCE);
-//					break;
-//				default:
-//					obj = null;
-//					break;
-//				}
 				return ds.getConnection();
 			}catch(Exception e){
 				e.printStackTrace();
 				throw new Exception(e.toString());
 			}
 		}
-		
 		/**
 		 * 
 		 * @param jndiName (For example, "jdbc/DS_LISDBS1D")
@@ -85,39 +67,6 @@ public class JDBCServiceLocator {
 			}
 		}
 	    
-	 /*
-	    public Connection getConnection(int vpdID) {
-	        try {
-	            String poolName = getPoolName(vpdID);
-	            InitialContext ctx = new InitialContext();
-	            log.debug("test jdbc");
-	            return ((DataSource) ctx.lookup(JAVA_ENV + poolName)).getConnection();
-	        } catch (SQLException e) {
-	            log.error("ERROR",e);
-	        } catch (Exception e) {
-	            log.error("ERROR",e);
-	        }
-	        return null;
-//	        try {
-//	            String poolName = getPoolName(vpdID);
-//		        ConnectionPool pool = (ConnectionPool) pools.get(poolName);
-//		        if (pool == null) {
-//		            int max = 100;
-//		            int init = 0;
-//		            boolean waitIfBusy = true;
-//		            InitialContext ctx = new InitialContext();
-//	                pool = new ConnectionPool(poolName, (DataSource) ctx.lookup(JAVA_ENV + poolName), init, max, waitIfBusy);
-//	                pools.put(poolName, pool);
-//		        }
-//	            return pool.getConnection();
-//	        } catch (SQLException e) {
-//	            log.error("ERROR",e);
-//	        } catch (Exception e) {
-//	            log.error("ERROR",e);
-//	        }
-//	        return null;
-	    }
-	*/
 	    public void freeConnection(int vpdID, Connection con) {
 	        try {
 		        if (!con.isClosed()) {
@@ -130,15 +79,5 @@ public class JDBCServiceLocator {
 	    }
 	    
 
-	    public static String getPoolName(int dbType) {
-	    	return MASTER_DATA_SOURCE;
-//	        switch (dbType) {
-//	        case MASTER_DB:
-//	            return MASTER_DATA_SOURCE;
-//	        default:
-//	            return MASTER_DATA_SOURCE;
-//	        }
-	    }
-	
 	}
 
